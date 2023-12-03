231630bdb690328b2d0cf7a8829f9c5a8dbe09cb
Mike George <mgeorge@ibm.com>
2023-12-03 02:32:13 -0500

    day 03 part 1

---
+++ 03/go.mod
@@ -0,0 +1,3 @@
+module github.com/migeorge/advent-of-code/03
+
+go 1.19
---
+++ 03/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	inputPath := flag.String("input", "", "input file path")
+	flag.Parse()
+
+	if len(*inputPath) == 0 {
+		fmt.Println("input flag must be provided")
+		os.Exit(1)
+	}
+
+	fileBytes, err := os.ReadFile(*inputPath)
+	if err != nil {
+		fmt.Println("unable to read input file:", err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Println("Part Number Sum:", SumInputs(fileBytes))
+}
